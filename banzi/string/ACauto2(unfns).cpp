3d834e1d26498a4b1dcc703e8c056c6306f15425
REXWindW <1114864532@qq.com>
2020-11-06 16:48:06 +0800

    acautomation

--- banzi/string/ACauto2(unfns).cpp
+++ banzi/string/ACauto2(unfns).cpp
@@ -1,74 +1,89 @@
 #include<iostream>
+#include<cstring>
+#include<algorithm>
 #include<queue>
 using namespace std;
+int cansel_sync=(ios::sync_with_stdio(0),cin.tie(0),0);
 #define rep(i,a,b) for(int i=(a);i<=(b);i++)
-const int MAXN = 77;
-inline int idx(char c){
-    return c-'a';
-}
+
+const int MAXN = 1e6+5;
+int jdbh[MAXN];//记录第i个模式串对应的节点编号
+int cntcx[MAXN];//记录第i个模式串出现的次数
+inline int idx(char c){return c-'a';}
 struct Node{
-    int son[25],fail,cnt;
-}trie[MAXN*26];
-int ans[MAXN*26];
-string s;
-int n,cntt;
-queue<int> q;
-void insert(string &s){
-    int siz = s.size();
-    int u=1,v;
+    int son[26],flag,fail;//cnt记录次数,flag记录编号
+    void clr(){
+        memset(son,0,sizeof(son));
+        flag=0;
+    }
+}trie[MAXN*10];
+int n,cntt;//cntt记录总点数
+string s,ms[166];
+int maxx;
+queue<int>q;
+void insert(string &s,int num){
+    int siz = s.size(),v,u=1;
     rep(i,0,siz-1){
         v = idx(s[i]);
-        if(!trie[u].son[v]) trie[u].son[v] = ++cntt;
+        if(!trie[u].son[v]){trie[u].son[v] = ++cntt;trie[cntt].clr();}
         u = trie[u].son[v];
     }
-    trie[u].cnt++;
+    trie[u].flag = num;//标记为单词,flag记录编号
+    //保证每个模式串只出现一次
+    cntcx[num] = 0;
+    jdbh[num] = u;//记录当前单词对应的节点编号
 }
 void getfail(){
     rep(i,0,25) trie[0].son[i] = 1;
-    int ufail,u = 1;
+    trie[0].flag = 0;
     q.push(1);
+    trie[1].fail = 0;
+    int u,v,ufail;
     while(!q.empty()){
         u = q.front();q.pop();
-        ufail = trie[u].fail;
         rep(i,0,25){
-            if(!trie[u].son[i]){
-                trie[u].son[i]=trie[ufail].son[i];
-                continue;
-            }
-            trie[trie[u].son[i]].fail = trie[ufail].son[i];
+            v = trie[u].son[i];
+            ufail = trie[u].fail;
+            if(!v){trie[u].son[i]=trie[ufail].son[i];continue;}//画好一条跳fail的路
+            trie[v].fail = trie[ufail].son[i];
+            q.push(v);
         }
     }
 }
-void query(string &s){
-    int siz = s.size();
-    int u=1,v,k;
+inline void query(string &s){
+    int siz = s.size(),u = 1,v,k;
     rep(i,0,siz-1){
         v = idx(s[i]);
         k = trie[u].son[v];
         while(k){
-            ans[k] += trie[k].cnt;
-            k = trie[k].fail;
+            if(trie[k].flag){
+                cntcx[trie[k].flag]++;//计数
+                maxx = max(maxx,cntcx[trie[k].flag]);
+            }
+            k = trie[k].fail;//跳fail
         }
         u = trie[u].son[v];
     }
 }
-void init(){
-    rep(i,0,cntt){//先前有几个点现在清几个点
-        trie[i].fail=trie[i].cnt = 0;
-        rep(j,0,25) trie[i].son[j]=0;
-    }
-}
 void solve(){
-    init();
+    cntt = 1;
+    trie[0].clr();
+    trie[1].clr();
     rep(i,1,n){
-        cin>>s;
-        insert(s);
+        cin>>ms[i];
+        insert(ms[i],i);
     }
+    getfail();
     cin>>s;
+    maxx = 0;
     query(s);
-    int maxx = 0;
-    
+    cout<<maxx<<endl;
+    rep(i,1,n){
+        if(cntcx[i]==maxx) cout<<ms[i]<<endl;
+    }
 }
 int main(){
     while(cin>>n&&n!=0) solve();
 }
+//洛谷 P3796 【模板】AC自动机（加强版）
+//https://www.luogu.com.cn/problem/P3796
