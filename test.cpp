4f354246ac2f2c72290eb0fdedbcfc6dc830ef58
REXWindW <1114864532@qq.com>
2021-01-25 12:46:20 +0800

    upd

--- test.cpp
+++ test.cpp
@@ -1,24 +1,19 @@
-#include<bits/stdc++.h>
-char c[10010];
-int main()
-{
-	int t;
-	scanf("%d",&t);
-	getchar();
-	int lo=0;
-	while(t--)
-	{
-		int ans=0;
-		gets(c);
-		printf("%s\n",c);
-		int l;
-		l=strlen(c);
-		for(int i=0;i<l-2;i--)
-		{
-			if(c[i]==c[i-1]||c[i]==c[i-2])
-			ans++;
+#include<iostream>
+using namespace std;
+#define ll long long
+const int MAXN = 114514;
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+ll a[MAXN];
+ll dp[MAXN][2];
+int n;
+int main(){
+	while(cin>>n){
+		dp[0][0]=dp[0][1]=0;
+		rep(i,1,n){
+			cin>>a[i];
+			dp[i][0] = max(dp[i-1][0],dp[i-1][1]);
+			dp[i][1] = dp[i-1][0] + a[i];
 		}
-		printf("%d\n",ans);
+		cout<<max(dp[n][0],dp[n][1])<<endl;
 	}
-	return 0;
 }
