adb733d1062adb8517f8e022f3a16f1ce35025cc
REXWindW <1114864532@qq.com>
2021-03-08 21:48:58 +0800

    buti

--- codeforces2021/Codeforces Round #705 (Div. 2)/C.cpp
+++ codeforces2021/Codeforces Round #705 (Div. 2)/C.cpp
@@ -10,97 +10,80 @@ int cansel_sync=(ios::sync_with_stdio(0),cin.tie(0),0);
 #define repb(i,a,b) for(int i=(a);i>=(b);i--)
 #define INF 0x3f3f3f3f
 ll gcd(ll a,ll b){ while(b^=a^=b^=a%=b); return a; }
-//把n分组
-//初期先贴着填,如果后来贴着填字母太多的话，就把最后一个贴着填的变成大一点的字母!
-//abcd这个，ab贴着填，c不够贴着了，而且c比之前出现的都大
+
 const int MAXN = 114514;
 string s;
 int n,k;
 int a[MAXN];
-int b[MAXN];
 int cnt[30];
+int sum[30][MAXN];//前缀和
 inline int idx(char c){return c-'a';}
-void fixx(){//填充剩余
+inline bool check(int pos){
+    if(a[pos]=='z'-'a') return 0;//z不能往上移动
+    int summ = 0;
+    sum[a[pos]][pos]--;
+    sum[a[pos]+1][pos]++;
     rep(i,0,28){
-        if(cnt[i]>=k){
-            cnt[0]+=(cnt[i]-cnt[i]%k);//全都回退到a
-            cnt[i]%=k;
+        summ+=(sum[i][pos]+k-1)/k*k;
+    }
+    //cout<<"check"<<pos<<"summ"<<summ<<endl;
+    sum[a[pos]][pos]++;
+    sum[a[pos]+1][pos]--;
+    if(summ<=n){
+        sum[a[pos]][pos]--;
+        sum[a[pos]+1][pos]++;
+        a[pos]=a[pos]+1;
+        return 1;
+    }
+    //也可以上探
+    if(summ-k<=n&&sum[a[pos]+1][pos]%k==0){//想办法减少一组
+        int up = 0;
+        rep(i,a[pos]+1,28){
+            if(sum[i][pos]%k){//前面还有剩的
+                up = i;break;
+            }
         }
+        if(up==0)return 0;
+        sum[a[pos]][pos]--;
+        sum[up][pos]++;
+        a[pos]=up;
+        return 1;
     }
-    int now = 0;
-    rep(i,0,n-1){
-        if(b[i]!=-1) continue;
-        while(cnt[now]==0) now++;
-        b[i]=now;
-        cnt[now]--;//填数
-    }
+    return 0;
 }
 void solve(){
-    rep(i,0,29) cnt[i]=0;
     cin>>n>>k;
     cin>>s;
-    //cout<<"!";
-    //-1情况
     if(n%k){
-        cout<<-1<<endl;return;
-    }
-    if(k==1){cout<<s<<endl;return;}
-    bool flag = 1;
-    rep(i,1,n-1)
-        if(s[i]!='z'){flag=0;break;}
-    if(flag){
         cout<<-1<<endl;
         return;
     }
-    //正常情况
-    rep(i,0,n-1){
-        a[i]=idx(s[i]);//转数字
-        b[i]=-1;//打上空标记
+    rep(j,0,28){cnt[j]=0;sum[j][0]=0;}
+    rep(i,1,n){
+        a[i]=idx(s[i-1]);
+        rep(j,0,28) sum[j][i]=sum[j][i-1];
+        sum[a[i]][i]++;//统计一下前缀和
     }
-    int zu = n/k;//有多少组
-    int prep=-1;//上一个新增组的起始位置和字样
-    rep(i,0,n-1){
-        if(cnt[a[i]]){//无需新增组
-            cnt[a[i]]--;
-            b[i]=a[i];
-        }
-        else if(cnt[a[i]]==0){//新增组
-            if(zu==0){//无法新增组,改动最近一处新增组,也可以是换一个大点的到这里
-                int upp = -1;
-                rep(j,a[i]+1,28)//往上找合适的组
-                    if(cnt[j]){upp=j;break;}
-                if(upp!=-1){//如果往上的组刚好有剩余！
-                    cnt[upp]--;
-                    b[i]=upp;//填上去
-                    fixx();
-                    break;
-                }
-                else{//如果往上一组没有剩余
-                    int tmp=b[prep];
-                    cnt[tmp]++;
-                    b[prep]=tmp+1;
-                    rep(j,prep+1,i-1){
-                        cnt[b[j]]++;//回退回来
-                        b[j]=-1;
-                    }
-                    cnt[tmp+1]+=cnt[tmp];//这个时候可能tmp+1会超过k,可以让组重新回去
-                    cnt[tmp]=0;
-                    cnt[tmp+1]--;
-                    //剩下来的全都转换成a
-                    fixx();
-                    break;
-                }
-            }
-            else{//增加新的一组
-                zu--;
-                cnt[a[i]]+=k;
-                b[i]=a[i];
-                cnt[a[i]]--;
-                prep=i;//记录最近一次新增的组位置,方便之后改的时候能尽量靠后使字典序尽可能小
-            }
-        }
+    bool flag = 1;
+    rep(i,0,28)
+        if(sum[i][n]%k){flag=0;break;}
+    if(flag){cout<<s<<endl;return;}//可以直接出s的情况
+    int res = 0;
+    repb(i,n,1) if(check(i)){res = i;break;}
+    int tot = n-res;
+    rep(i,0,28){
+        cnt[i]=(sum[i][res]+k-1)/k*k-sum[i][res];
+        tot -= cnt[i];
     }
-    rep(i,0,n-1) cout<<(char)('a'+b[i]);
+    if(tot%k!=0){cout<<"彩笔，你算法写假了"<<endl;}
+    cnt[0]+=tot;
+    int now=0;
+    rep(i,res+1,n){
+        while(cnt[now]==0) now++;
+        a[i]=now;
+        cnt[now]--;
+    }
+    rep(i,1,n) cout<<(char)(a[i]+'a');
     cout<<endl;
 }
 
--- codeforces2021/Codeforces Round #705 (Div. 2)/D.cpp
+++ codeforces2021/Codeforces Round #705 (Div. 2)/D.cpp
@@ -3,6 +3,7 @@
 #include<cstdio>
 #include<map>
 #include<vector>
+#include<cmath>
 #include<algorithm>
 using namespace std;
 
@@ -12,28 +13,69 @@ int cansel_sync=(ios::sync_with_stdio(0),cin.tie(0),0);
 #define repb(i,a,b) for(int i=(a);i>=(b);i--)
 #define INF 0x3f3f3f3f
 ll gcd(ll a,ll b){ while(b^=a^=b^=a%=b); return a; }
+
+const int med = 1e9+7;
 const int MAXN = 2e5+5;
 bool prim[MAXN];
-vector<int> primm;
-map<int> mp[MAXN];//第i个位置有几个质数j
-map<int> cp[MAXN];//质数j在几个位置出现了i次以上？
+vector<int> prm;
+map<int,int> mp[MAXN];//第i个位置有几个质数j
+map<int,int> cp[MAXN];//质数j在几个位置出现了i次？
 void ass(){
-    rep(i,2,MAXN-5){m 
+    rep(i,2,MAXN-5){
         if(prim[i]) continue;
-        primm.push_back(i);
+        prm.push_back(i);
         for(int j=i*2;j<=MAXN-5;j+=i)
             prim[j]=1;
     }
-    cout<<primm.size()<<endl;
+    //cout<<primm.size()<<endl;
 }
-
+int n,q;
+ll gcdd;
+int hc;
 void solve(){
-    
+    gcdd = 1;
+    cin>>n>>q;
+    rep(i,1,n){
+        cin>>hc;
+        int sqr = sqrt(hc);
+        for(int j=0;prm[j]<=sqr;j++){
+            while(hc%prm[j]==0){
+                hc/=prm[j];
+                mp[i][prm[j]]++;
+                cp[prm[j]][ mp[i][prm[j]] ]++;
+                if(cp[prm[j]][ mp[i][prm[j]] ]==n)gcdd*=prm[j];
+            }
+        }
+        if(hc!=1){
+            mp[i][hc]++;
+            cp[hc][mp[i][hc]]++;
+            if(cp[hc][mp[i][hc]]==n) gcdd*=hc;
+        }
+    }
+    int pos,x;
+    int sqr;
+    while(q--){
+        cin>>pos>>x;
+        sqr = sqrt(x);
+        for(int j=0;prm[j]<=sqr;j++){
+            while(x%prm[j]==0){
+                x/=prm[j];
+                mp[pos][prm[j]]++;
+                cp[prm[j]][ mp[pos][prm[j]] ]++;
+                if(cp[prm[j]][ mp[pos][prm[j]] ]==n)gcdd=gcdd*prm[j]%med;
+            }                
+            
+        }
+        if(x!=1){
+            mp[pos][x]++;
+            cp[x][mp[pos][x]]++;
+            if(cp[x][mp[pos][x]]==n)gcdd=gcdd*x%med;
+        }
+        cout<<gcdd<<endl;
+    }
 }
 
 int main(){
     ass();
-    int z;
-    cin>>z;
-    while(z--) solve();
+    solve();
 }
